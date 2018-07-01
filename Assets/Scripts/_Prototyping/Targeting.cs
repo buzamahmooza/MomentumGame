1c80111ee63ddadd9f3e388d69531940e4c0a8f3
Faris H <buzamahmooza@gmail.com>
2018-07-01 13:29:12 +0300

    Created Targeting superclass
    Contains a Target and AimDirection properties
    AimInput inherits from this class

---
+++ Assets/Scripts/_Prototyping/Targeting.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Targeting : MonoBehaviour
+{
+    [SerializeField] public Transform Target;
+
+    public virtual Vector2 AimDirection {
+        get {
+            return Target ?
+                (Vector2)(Target.transform.position - transform.position) :
+                Vector2.right;
+        }
+    }
+}
