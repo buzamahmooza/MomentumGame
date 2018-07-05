8d2474dbedc57f0be17ea3507d65aca9a340af83
Faris H <buzamahmooza@gmail.com>
2018-07-05 10:59:29 +0300

    Getting score now increases your momentum

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,6 +6,7 @@ public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private UnityEngine.UI.Text scoreText;
     private float currentScore = 0;
+    [SerializeField] [Range(0, 100)] private float scoreAdded2MomentumPercent;
 
     // Use this for initialization
     void Start() {
@@ -16,9 +17,14 @@ public class ScoreManager : MonoBehaviour
     void Update() {
 
     }
-
+    /// <summary>
+    /// Adds the score to the current score value and updates the score text
+    /// Also adds momentum with a value of x% of the addedScore
+    /// </summary>
+    /// <param name="addedScore"></param>
     public void AddScore(float addedScore) {
         if (addedScore < 0) addedScore = 0;
+        GameManager.Player.GetComponent<PlayerMove>().AddMomentum(addedScore * scoreAdded2MomentumPercent / 100.0f);
 
         currentScore += addedScore;
         UpdateScore();
