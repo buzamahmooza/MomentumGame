71e43f849f62595ef15031c013f9635b53d9e6cf
Unknown <buzamahmooza@gmail.com>
2018-07-21 13:04:31 +0300

    Created Melee enemy and Skeleton enemy

---
+++ Assets/MeleeEnemy.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+/// <summary>
+/// The enemy will lunge once the attack is ready, and then another tiny lunge when the attackOpens just to get closer
+/// </summary>
+public class MeleeEnemy : Enemy
+{
+    [SerializeField] private EnemyHitbox attackHitbox;
+    [SerializeField] [Range(0, 10)] private float lungeForce = 2f;
+    private bool attackReady = false;
+    private float meleeRange = 1f;
+
+    protected override void Awake() {
+        base.Awake();
+        if (!attackHitbox) attackHitbox = GetComponent<EnemyHitbox>() ?? GetComponentInChildren<EnemyHitbox>();
+
+        print("meleeRange = " + meleeRange);
+    }
+
+    protected override void Start() {
+        base.Start();
+        if (attackHitbox)
+            meleeRange = attackHitbox.collider2D.bounds.extents.magnitude +
+                         Vector2.Distance(attackHitbox.transform.position, transform.position);
+        attackHitbox.collider2D.enabled = false;
+    }
+
+    /// <summary>
+    /// overriding Attack() so it won't play the attack sound
+    /// </summary>
+    public override void Attack() {
+        if (!m_CanAttack) return;
+        if (m_Attacking) return;
+
+        m_Attacking = true;
+        _anim.SetTrigger("Attack");
+        FaceAimDirection();
+    }
+
+    /// <summary>
+    /// Jumps to the player direction so he'd get hurt. Just like COD knifing
+    /// </summary>
+    private void Lundge(float mult) {
+        rb.AddForce(targeting.AimDirection * lungeForce * mult * rb.mass, ForceMode2D.Impulse);
+    }
+
+    protected override void Update() {
+        base.Update();
+
+        // if attack is ready, attack as soon as the player is within melee range
+        if (attackReady && m_Attacking && targeting.AimDirection.magnitude < meleeRange) {
+            attackReady = false;
+            _anim.speed = m_DefaultAnimSpeed;
+        }
+    }
+
+    // called by animationEvents
+    /// <summary>
+    /// Called when the attackIsReady to happen
+    /// (for example an enemy raising a sword, when the sword is raised, he's ready to attack)
+    /// </summary>
+    public void AttackReady() {
+        attackReady = true;
+        _anim.speed = 0;
+        Lundge(1f);
+        StartCoroutine(AttackReadyTimeout());
+    }
+    /// <summary>
+    /// This will get the enemy unstuck from the position of holding his waepon up, waiting for the player to be in range.
+    /// The enemy will only wait so long
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator AttackReadyTimeout() {
+        yield return new WaitForSeconds(0.6f);
+        attackReady = false;
+        _anim.speed = m_DefaultAnimSpeed;
+    }
+    public void ActivateHitbox() {
+        Lundge(0.4f);
+        attackHitbox.collider2D.enabled = true;
+        if (attackSound) audioSource.PlayOneShot(attackSound);
+    }
+    public void CloseHitbox() {
+        attackHitbox.collider2D.enabled = false;
+        m_Attacking = false;
+        attackReady = false;
+    }
+
+    protected override void OnDrawGizmos() {
+        base.OnDrawGizmos();
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyHitbox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class EnemyHitbox : MonoBehaviour
+{
+    [SerializeField] [Range(0, 1000)] public int damageAmount = 20;
+    [SerializeField] private AudioClip hitSound;
+    [HideInInspector] public new Collider2D collider2D;
+    private AudioSource audioSource;
+    private void OnEnable() { collider2D.enabled = true; }
+    private void OnDisable() { collider2D.enabled = false; }
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+        if (!audioSource) audioSource = GetComponentInParent<AudioSource>();
+        collider2D = GetComponent<Collider2D>();
+        collider2D.enabled = false;
+    }
+    private void Start() {
+        if (!transform.parent)
+            return;
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (transform.IsChildOf(other.gameObject.transform)) // prevent damaging the attacker
+            return;
+
+        Health otherHealth = other.gameObject.GetComponent<Health>();
+        // if it's NOT the same type as the attacker, do damage (so enemies can't damage eachother)
+        if (otherHealth && !other.CompareTag(transform.parent.tag)) {
+            otherHealth.TakeDamage(damageAmount);
+            if (hitSound) audioSource.PlayOneShot(hitSound);
+        }
+    }
+}
