4302c9255f4497d4d0fd1dfd923594a9d6371200
Unknown <buzamahmooza@gmail.com>
2018-07-24 01:18:41 +0300

    Created Utils class + cleaning code

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Utils
+{
+    /// <summary>
+    /// cannot be instantiated
+    /// </summary>
+    private Utils() { }
+
+    /// <summary> Returns true if the given LayerMask contains the given layer </summary>
+    /// <param name="layerMask"></param>
+    /// <param name="layer"></param>
+    /// <returns></returns>
+    public static bool IsInLayerMask(LayerMask layerMask, int layer) {
+        return (layerMask.value & 1 << layer) != 0;
+    }
+}
