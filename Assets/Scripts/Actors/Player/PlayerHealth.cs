9f02e0a71c265318cc271762664218164c2c66b0
Unknown <buzamahmooza@gmail.com>
2018-08-10 17:31:33 +0300

    Now health objects can have multiple Hurt audioClips instead of one
    Replaced the hurt audioClip by an array where a clip is chosen randomly

--- Assets/Scripts/Actors/Player/PlayerHealth.cs
+++ Assets/Scripts/Actors/Player/PlayerHealth.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using UnityEngine;
-using UnityEngine.TestTools;
 using UnityEngine.UI;
 
 public class PlayerHealth : Health
@@ -23,13 +22,33 @@ public class PlayerHealth : Health
         _anim.SetBool("Dead", true);
 
         //disable all scriptstry 
-        GetComponent<GrappleHook>().EndGrapple();
+        var grappleHook = GetComponent<GrappleHook>();
+        if (grappleHook) grappleHook.EndGrapple();
 
         SpriteRenderer.color = Color.white;
 
-        Destroy(GetComponent<Walker>());
-        Destroy(GetComponent<Shooter>());
+        try
+        {
+            Destroy(GetComponent<GrappleHook>());
+            Destroy(GetComponent<PlayerAttack>());
+//            Destroy(GetComponent<PlayerMove>());
+            Destroy(GetComponent<Shooter>());
+//            Destroy(GetComponent<Health>());
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Caught: " + e);
+        }
+
         foreach (MonoBehaviour script in GetComponents<MonoBehaviour>())
-           Destroy(script);
+            try
+            {
+                script.enabled = false;
+//                Destroy(script);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Caught: " + e);
+            }
     }
 }
